604ba52abdf1793f3ad9b4d674b525026c9b54d2
YawarPandar <mf.ramirezl@gmail.com>
2021-06-13 23:29:27 -0500

    Correccion scripts

--- SQL_Script/DB_Objects_v1.sql
+++ SQL_Script/DB_Objects_v1.sql
@@ -130,15 +130,21 @@ begin
 	delete a from pe_Actas a
 		inner join pe_Mesas m
 			on a.mesa_numero = m.mesa_numero
+			and a.eleccion = m.eleccion
 	where m.local_ubigeo like @level + '%'
+	and a.eleccion = @eleccion
 
 	delete v from pe_Votos v
 		inner join pe_Mesas m
 			on v.mesa_numero = m.mesa_numero
+			and v.eleccion = m.eleccion
 	where m.local_ubigeo like @level + '%'
+	and v.eleccion = @eleccion
 
-	delete from pe_Locales where local_ubigeo like @level + '%'
+	delete from pe_Locales 
+	where local_ubigeo like @level + '%' and eleccion = @eleccion
 
-	delete from pe_Mesas where local_ubigeo like @level + '%'
+	delete from pe_Mesas 
+	where local_ubigeo like @level + '%' and eleccion = @eleccion
 end
 GO
